116b6394b296d5480ba4322e47ef4eb8065a99b8
mr. whalen <michael.whalen@sitechhs.com>
2021-12-22 08:37:56 -0500

    done

--- vue-basics/src/views/Home.vue
+++ vue-basics/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <h1>Hello</h1>
+    <h1 v-if="graduated">{{ student }}</h1>
+    <h1 v-else>"They did not yet graduate"</h1>
   </div>
 </template>
 
@@ -10,6 +11,12 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      student: "Harry",
+      graduated: true,
+    };
+  },
 };
 </script>
 <style>
